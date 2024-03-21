0cec491bf51ce079e0d7be92a48056bb66ab422a
Shnehi K <88911284+Sk2195@users.noreply.github.com>
2024-03-20 20:25:51 -0700

    Update main.py

--- tasks/Task 5- App Deployment/main.py
+++ tasks/Task 5- App Deployment/main.py
@@ -1,11 +1,9 @@
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from PIL import Image
 import joblib
-import warnings
-
+import matplotlib.pyplot as plt
+from PIL import Image
+import pandas as pd
+import seaborn as sns
+import streamlit as st
 
 # Disable this warning by disabling the config option:
 
